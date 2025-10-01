7712eebbe6fbf0857b520c730c9060e37b03b2da
kaushal-waygood <kaushal@waygood.in>
2025-10-01 08:38:16 -0400

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,12 +2,12 @@
 
 import axios from 'axios';
 
-console.log('API_BASE_URL:', process.env.NEXT_PUBLIC_NODE_ENV);
-
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_NODE_ENV === 'production'
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 
+  (process.env.NEXT_PUBLIC_NODE_ENV === 'production'
     ? 'https://api.zobsai.com'
-    : 'http://api.dev.zobsai.com';
+    : 'https://api.dev.zobsai.com'); // use HTTPS for dev environment
+
+console.log('API_BASE_URL:', API_BASE_URL);
 
 const safeLocalStorage = {
   getItem: (key: string): string | null => {
