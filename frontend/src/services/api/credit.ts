c519fbfe351aefe43591defd6e8e0cb0a3024a58
sandeep <sandeep@helpstudyabroad.com>
2025-11-26 17:02:43 +0530

    feat: Update dashboard tour popovers, enhance footer with social links, and clean up credit API logging

--- frontend/src/services/api/credit.ts
+++ frontend/src/services/api/credit.ts
@@ -2,6 +2,6 @@ import apiInstance from '../api';
 
 export const getCredit = async () => {
   const response = await apiInstance.get(`/students/credits`);
-  console.log('response', response);
+
   return response;
 };
