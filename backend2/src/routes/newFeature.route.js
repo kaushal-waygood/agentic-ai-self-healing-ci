d21aef87bc0746375ba65207be0968d5f048cbfd
arsalan <arsalan@helpstudyabroad.com>
2025-12-31 15:04:44 +0530

    chore: Remove the entire backend2 application and its associated files.

--- backend2/src/routes/newFeature.route.js
+++
@@ -1,9 +0,0 @@
-import { requestNewFeature } from '../controllers/newFeature.controller.js';
-import { Router } from 'express';
-import { authMiddleware, isStudent } from '../middlewares/auth.middleware.js';
-
-const router = Router();
-
-router.post('/', authMiddleware, isStudent, requestNewFeature);
-
-export default router;
--- backend2/tests/config/constants.js
+++
@@ -1,6 +0,0 @@
-export default {
-    // BASE_URL: "http://localhost:8080",
-    BASE_URL: "https://api.dev.zobsai.com",
-    ACCESS_TOKEN: "",
-    REFRESH_TOKEN: "",
-};
