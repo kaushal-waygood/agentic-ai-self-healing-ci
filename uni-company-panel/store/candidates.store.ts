7b638f62a006f49a8a3d86dc03bfbdacaeb7d010
arsalan <arsalan@helpstudyabroad.com>
2026-02-04 16:55:15 +0530

    feat: Implement pagination for hosted jobs and candidates, updating API responses and frontend data handling.

--- uni-company-panel/store/candidates.store.ts
+++ uni-company-panel/store/candidates.store.ts
@@ -20,19 +20,30 @@ interface CandidateStore {
 const useCandidateStore = create<CandidateStore>((set, get) => ({
   candidates: [],
   candidatesStats: null,
+  meta: null,
   loading: false,
 
   pagination: { totalCount: 0, totalPages: 0, currentPage: 1 },
   getCandidates: async (jobId: string) => {
     try {
       set({ loading: true });
-      // const response = await apiInstance.get(`/jobs/candidates-organization`);
+
       const response = await apiInstance.get(
         `/jobs/hosted/jobs/candidates/${jobId}`,
       );
-      set({ candidates: response.data });
+
+      const { data, meta } = response.data;
+
+      set({
+        candidates: data ?? [],
+        meta: meta ?? null,
+      });
     } catch (error) {
       console.error('Error fetching candidates:', error);
+      set({
+        candidates: [],
+        meta: null,
+      });
     } finally {
       set({ loading: false });
     }
