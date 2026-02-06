61d40f5d84b752f784638ad2a23f9ac07ac24729
arsalan <arsalan@helpstudyabroad.com>
2026-02-06 10:44:08 +0530

    feat: Implement analytics dashboard with dedicated backend and frontend components, and enhance `uni-company-panel` UI with new Radix-UI sheet and location pickers.

--- super-admin-dashboard/app/dashboard/page.tsx
+++ super-admin-dashboard/app/dashboard/page.tsx
@@ -1,9 +1,12 @@
+import AnalyticsDashboard from '@/components/dashboard/analytics/dashboard';
 import React from 'react';
 
 const page = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <AnalyticsDashboard />
+    </div>
+  );
 };
 
 export default page;
