fbeb172706f1caaf6e9e67a7a7b6dcdaf97d4b3e
sandeep <sandeep@helpstudyabroad.com>
2026-02-16 18:32:46 +0530

    remove 40 unused files

--- frontend/src/services/api/jobRole.ts
+++
@@ -1,9 +0,0 @@
-import apiInstance from '../api';
-
-export const createJobRole = async (payload: {
-  title: string;
-})=>{
-    const response = await apiInstance.post('/jobRole/create', payload);
-    return response;
-}
-
