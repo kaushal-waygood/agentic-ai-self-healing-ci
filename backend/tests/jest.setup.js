fcbe42f825ab9ee42e61a0f25b57b7b600ef600c
arsalan <arsalan@helpstudyabroad.com>
2026-03-08 08:01:50 +0530

    Mock Redis client in Jest setup to prevent CI connection issues during unit tests, enhancing test isolation and reliability.

--- backend/tests/jest.setup.js
+++ backend/tests/jest.setup.js
@@ -1,6 +1,28 @@
 import constants from './config/constants.js';
 import mongoose from 'mongoose';
 
+// Mock Redis so unit tests don't connect (no Redis in CI)
+jest.mock('../src/config/redis.js', () => {
+  const noop = () => Promise.resolve();
+  const mockClient = {
+    get: () => Promise.resolve(null),
+    set: noop,
+    del: noop,
+    keys: () => Promise.resolve([]),
+    scanKeys: () => Promise.resolve([]),
+    mget: () => Promise.resolve([]),
+    mset: noop,
+    withCache: async (_key, _ttl, callback) => callback(),
+    invalidateJobCache: noop,
+    invalidateStudentCache: noop,
+    invalidateUserCache: noop,
+    invalidateJobCacheForStudent: noop,
+    invalidateAllJobsCache: noop,
+    disconnect: noop,
+  };
+  return { __esModule: true, default: mockClient };
+});
+
 beforeAll(() => {
   constants.ACCESS_TOKEN = 'some-valid-token';
   constants.REFRESH_TOKEN = 'some-refresh-token';
@@ -10,12 +32,5 @@ afterAll(async () => {
   // Close Mongoose connection (prevents "worker failed to exit" when API tests run)
   if (mongoose.connection.readyState !== 0) {
     await mongoose.connection.close();
-  }
-  // Close Redis if it was loaded (e.g. by jobHelpers, controllers)
-  try {
-    const redis = (await import('../src/config/redis.js')).default;
-    if (redis?.disconnect) await redis.disconnect();
-  } catch {
-    // Redis may not be loaded in this worker
   }
 });
