8b273e28ac12f632be2ce5273558a792ef2db30b
arsalan <arsalan@helpstudyabroad.com>
2026-03-27 09:06:22 +0530

    feat: Replace font styles with Plus Jakarta Sans and update related configurations

--- frontend/src/app/fonts.ts
+++ frontend/src/app/fonts.ts
@@ -1,16 +1,8 @@
-// app/fonts.js
-import { Poppins, PT_Sans } from 'next/font/google';
+import { Plus_Jakarta_Sans } from 'next/font/google';
 
-export const poppins = Poppins({
+export const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ['latin'],
   display: 'swap',
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-poppins', // Use CSS variable for the main body font
-});
-
-export const pt_sans = PT_Sans({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['400', '700'],
-  variable: '--font-pt-sans', // Optional: for headings or specific elements
+  weight: ['400', '500', '600', '700', '800'],
+  variable: '--font-plus-jakarta',
 });
